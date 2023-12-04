0d184716afba3dcb0dd1383282cccff867457cf6
MrLous <ricardoluizjr@live.com>
2023-12-03 22:40:38 -0300

    finalizando stelly, inicio engine

---
+++ src/scripts/engine.js
@@ -0,0 +1,56 @@
+const stant = {
+    score:{
+        playerScore:0,
+        computerScore:0,
+        scoreBox: document.getElementById("sacore_points")
+    },
+    cardSprites:{
+        avatar: document.getElementById("card-image"),
+        name: document.getElementById("carde-name"),
+        type: document.getElementById("card-type")
+    },
+    fieldCards:{
+        player: document.getElementById("player-field-card"),
+        computer: document.getElementById("computer-field-card")
+    },
+    actions:{
+        button: document.getElementById("next-duel")
+    }
+};
+
+const player = {
+    player1: "player-cards",
+};
+
+const pathImages = ".src/assets/icon/";
+
+const cardData = [
+    {
+        id:0,
+        name: "Blue Eyes White Dragon",
+        type: "Paper",
+        img: pathImages + "dragon.png",
+        WinOf: [1],
+        LoseOf: [2]
+    },{
+        id:1,
+        name: "Dark Magician",
+        type: "Rock",
+        img: pathImages + "magician.png",
+        WinOf: [2],
+        LoseOf: [0]
+    },{
+        id:2,
+        name: "Exodia",
+        type: "Scissors",
+        img: pathImages + "exodia.png",
+        WinOf: [0],
+        LoseOf: [1]
+    }
+];
+
+function init() {
+
+};
+
+init();
