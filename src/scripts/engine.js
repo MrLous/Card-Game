fc61e884409ea4c65b44b8175d161f6d72fae7b1
MrLous <ricardoluizjr@live.com>
2023-12-04 23:26:03 -0300

    add funções e animações

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,4 +1,6 @@
-const stant = {
+const pathImages = "./src/assets/icons/";
+
+const state = {
     score:{
         playerScore:0,
         computerScore:0,
@@ -6,8 +8,10 @@ const stant = {
     },
     cardSprites:{
         avatar: document.getElementById("card-image"),
-        name: document.getElementById("carde-name"),
-        type: document.getElementById("card-type")
+        name: document.getElementById("card-name"),
+        type: document.getElementById("card-type"),
+        lost: document.getElementById("lost"),
+        win: document.getElementById("win"),
     },
     fieldCards:{
         player: document.getElementById("player-field-card"),
@@ -18,11 +22,10 @@ const stant = {
     }
 };
 
-const player = {
-    player1: "player-cards",
+const playerSides = {
+    player: "player-card",
+    computer: "computer-card",
 };
-
-const pathImages = ".src/assets/icon/";
 
 const cardData = [
     {
@@ -30,27 +33,100 @@ const cardData = [
         name: "Blue Eyes White Dragon",
         type: "Paper",
         img: pathImages + "dragon.png",
-        WinOf: [1],
-        LoseOf: [2]
+        WinOf: "Rock",
+        LoseOf: "Scissors"
     },{
         id:1,
         name: "Dark Magician",
         type: "Rock",
         img: pathImages + "magician.png",
-        WinOf: [2],
-        LoseOf: [0]
+        WinOf: "Scissors",
+        LoseOf: "Paper"
     },{
         id:2,
         name: "Exodia",
         type: "Scissors",
         img: pathImages + "exodia.png",
-        WinOf: [0],
-        LoseOf: [1]
+        WinOf: "Paper",
+        LoseOf: "Rock"
     }
 ];
 
-function init() {
+async function getRandomCardId(){
+    const randomIndex = Math.floor(Math.random() * cardData.length);
+    return randomIndex;
+}
 
+async function drawSelectCard(index){
+    state.cardSprites.avatar.src = cardData[index].img;
+    state.cardSprites.name.innerText = cardData[index].name;
+    state.cardSprites.type.innerText = "Attribute : "+cardData[index].type;
+    state.cardSprites.win.innerText = "Wins from : "+cardData[index].WinOf;
+    state.cardSprites.lost.innerText = "Lose to : "+cardData[index].LoseOf;
+}
+
+async function checkDuelResults(playerCardId,computerCardId){
+
+}
+
+async function setCardFild(playerCardId){
+    await removeAllCardImage();
+
+    let computerCardId = await getRandomCardId();
+
+    state.fieldCards.player.style.display = "block";
+    state.fieldCards.computer.style.display = "block";
+
+    state.fieldCards.player.src = cardData[playerCardId].img;
+    state.fieldCards.computer.src = cardData[computerCardId].img;
+
+    let duelResults = await checkDuelResults(playerCardId, computerCardId);
+
+    await updateScore();
+    await drawButton(duelResults);
+}
+
+async function creatCardImage(IdCard, fildSide){
+
+    const cardImg = document.createElement("img");
+
+    cardImg.setAttribute("height", "100px");
+    cardImg.setAttribute("src", pathImages+"card-back.png");
+    cardImg.setAttribute("data-id", IdCard);
+
+    if(fildSide === playerSides.player){
+        
+        cardImg.classList.add("card");
+
+        cardImg.addEventListener("click", () => {
+            setCardFild(cardImg.getAttribute("data-id"));
+        });
+
+        cardImg.addEventListener("mouseover", () => {
+            drawSelectCard(IdCard);
+            cardImg.setAttribute("src",cardData[IdCard].img);
+        });
+        cardImg.addEventListener("mouseout", () => {
+            cardImg.setAttribute("src", pathImages+"card-back.png");
+        });
+    };
+
+    return cardImg;
+
+}
+
+async function drawCard(cardNumbers, fildSide){
+    for (let i = 0; i < cardNumbers; i++){
+        const randomIdCard = await getRandomCardId();
+        const cardImg = await creatCardImage(randomIdCard, fildSide);
+
+        document.getElementById(fildSide).appendChild(cardImg);
+    }
+}
+
+function init() {
+    drawCard(5, playerSides.player);
+    drawCard(5, playerSides.computer);
 };
 
 init();
